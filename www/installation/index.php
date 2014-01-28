4f1d7f947538253cc410ea1b1a2a0b4a32aea934
Júlio Pontes <juliopfneto@gmail.com>
2014-01-28 12:15:08 -0300

    fix installation

--- www/installation/index.php
+++ www/installation/index.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 
 define('JPATH_ROOT', dirname(dirname(__DIR__)).'/installer');
 define('JPATH_ADMINISTRATOR', dirname(dirname(__DIR__)).'/administrator');
+define('JPATH_SITE', , dirname(dirname(__DIR__)).'/frontend');
 
 require_once '../includes/defines.php';
 
